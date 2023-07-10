c964ebaa9c1d3c15cceb189430ea5ca178d70aa8
Oleg Rybin <73353598+orybin479@users.noreply.github.com>
2023-07-10 01:06:55 -0400

    Add files via upload

---
+++ RMCharcterStatus.swift
@@ -0,0 +1,18 @@
+//
+//  RMCharcterStatus.swift
+//  RIckAndMorty
+//
+//  Created by Oleg Rybin on 3/29/23.
+//
+
+import Foundation
+
+enum RMCharcterStatus: String, Codable {
+    // Alive Dead or Unkown set by api
+    case alive = "Alive"
+    case dead = "Dead"
+    case `unkown` = "unkown"
+}
+
+var oleg = 5
+
